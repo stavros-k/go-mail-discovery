163609cddc701d2dab9d5d708bb3c19fc0be901b
Stavros kois <s.kois@outlook.com>
2024-08-22 11:38:06 +0300

    updates

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/stavros-k/go-mail-discovery/internal/handlers"
 	"github.com/stavros-k/go-mail-discovery/internal/providers"
 )
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/stavros-k/go-mail-discovery
 
-go 1.22.3
+go 1.23.0
 
 require (
 	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
 	github.com/spf13/cobra v1.8.1
 	golang.org/x/net v0.28.0
 )
